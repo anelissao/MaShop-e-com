3533bc07ecfc0c0379052b4d91d577bc8a3d6c7a
anelissao <anouarelissaoui20@gmail.com>
2026-05-02 20:07:58 +0100

    initialize backend and did some test for the running commands with nodemon concurrently

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import products from './products.js';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const Port = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Api is running');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/product/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product)
+})
+
+app.listen(Port, () => console.log(`Backend is running on Port ${Port}`));
