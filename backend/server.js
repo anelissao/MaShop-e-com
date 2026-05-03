eeedabda41117b56ad77009104fcbc55691f4409
anelissao <anouarelissaoui20@gmail.com>
2026-05-03 02:41:07 +0100

    use mongodb atlas and mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import products from './products.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+connectDB();
 const Port = process.env.PORT || 5000;
 const app = express();
 
