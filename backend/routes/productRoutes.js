b1c7575fe0918b86599ee0f65e24b15725d746d2
anelissao <anouarelissaoui20@gmail.com>
2026-05-03 22:15:56 +0100

    fetching products from database

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import asyncHandler from '../middleware/asyncHandler';
+import Product from '../models/productModel';
 
 router.get(
   '/',
   asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
   }),
 );
@@ -12,7 +14,7 @@ router.get(
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
+    const product = await Product.findById(req.params.id)
     res.json(product);
   }),
 );
