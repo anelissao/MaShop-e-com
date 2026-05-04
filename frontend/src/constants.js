1289df238efa790e80f71a849bc5450a2e13fb75
anelissao <anouarelissaoui20@gmail.com>
2026-05-04 22:03:31 +0100

    fixed the baseurl constant

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,8 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : '';
+//export const BASE_URL =
+//  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : '';
+
+// i gave the baseurl empty string bcz we already use a proxy (see package.json)
+export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/USERS';
 export const ORDERS_URL = '/api/orders';
